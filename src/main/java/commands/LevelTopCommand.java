0469e15c0790986ede0656dba4e5138d9e368a23
Ethan <wolffang404@gmail.com>
2020-02-08 22:33:01 -0600

    added level commands

---
+++ src/main/java/commands/LevelTopCommand.java
@@ -0,0 +1,45 @@
+package commands;
+
+import data.Data;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
+import java.sql.ResultSet;
+
+public class LevelTopCommand {
+
+    public static void command(GuildMessageReceivedEvent e, String message[]){
+        ResultSet rs = Data.getTopLevels();
+
+        String first = ":first_place:";
+        String second = ":second_place:";
+        String third = ":third_place:";
+
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setColor(Color.CYAN);
+        eb.setTitle("Top 5 Levels");
+        int count = 1;
+        try{
+            String userRank = "";
+            while(rs.next()){
+                switch (count){
+                    case 1: userRank = "**" + (count) + " - " + rs.getString("userName") + "**" + first;
+                        break;
+                    case 2: userRank = "**" + (count) + " - " + rs.getString("userName") + "**" + second;
+                        break;
+                    case 3: userRank = "**" + (count) + " - " + rs.getString("userName") + "**" + third;
+                        break;
+                    default: userRank = "**" + (count) + " - " + rs.getString("userName") + "**";
+                }
+
+                eb.addField(userRank, "> Level: " + rs.getInt("userLevel"), false);
+                count++;
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        e.getChannel().sendMessage(eb.build()).queue();
+    }
+}
